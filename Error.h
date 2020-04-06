a42f18cf3f6fe1c825229c824673ec8b24d1709a
Gast91 <dimkaz13@hotmail.com>
2020-04-06 05:06:44 +0100

    const ref strings

--- Error.h
+++ Error.h
@@ -4,17 +4,17 @@
 class UnexpectedTokenException : public std::exception
 {
 public:
-	UnexpectedTokenException(std::string msg) : exception(("PARSER ERROR: " + msg).c_str()) {}
+	UnexpectedTokenException(const std::string& msg) : exception(("PARSER ERROR: " + msg).c_str()) {}
 };
 
 class SymbolNotFoundException : public std::exception
 {
 public:
-	SymbolNotFoundException(std::string msg) : exception(("SEMANTIC ANALYZER ERROR: " + msg).c_str()) {}
+	SymbolNotFoundException(const std::string& msg) : exception(("SEMANTIC ANALYZER ERROR: " + msg).c_str()) {}
 };
 
 class SymbolRedefinitionException : public std::exception
 {
 public:
-	SymbolRedefinitionException(std::string msg) : exception(("SEMANTIC ANALYZER ERROR: " + msg).c_str()) {}
+	SymbolRedefinitionException(const std::string& msg) : exception(("SEMANTIC ANALYZER ERROR: " + msg).c_str()) {}
 };
