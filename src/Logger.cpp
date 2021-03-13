9a4cbb7140c1f396198cc31da11c83d50797e776
Gast91 <dimkaz13@hotmail.com>
2021-03-13 18:17:43 +0200

    Seperate static logging level functions, colors for each and using menu bars rather than buttons for the logging interface

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -17,12 +17,15 @@ void Logger::Clear()
 {
     buffer.clear();
     lineOffsets.clear();
-    lineOffsets.push_back(0);
+    lineOffsets.push_back({ 0, color[static_cast<int>(Level::DEBUG)] });
 }
 
 void Logger::Log(const Level lvl, const char* fmt, ...) IM_FMTARGS(2)
 {
-    if (static_cast<int>(lvl) < logLevel) return;
+    if (static_cast<int>(lvl) < logLevel || lvl > Level::ERROR) return;
+
+    // Each line MUST be its own thing, else colors get scrambled FIX-ME? 
+    lineOffsets.back().color = color[static_cast<int>(lvl)];
 
     int old_size = buffer.size();
     va_list args;
@@ -32,70 +35,82 @@ void Logger::Log(const Level lvl, const char* fmt, ...) IM_FMTARGS(2)
     for (int new_size = buffer.size(); old_size < new_size; old_size++)
     {
         if (buffer[old_size] == '\n')
-            lineOffsets.push_back(old_size + 1);
+            lineOffsets.push_back({ old_size + 1, color[static_cast<int>(lvl)] });
     }
 }
 
 void Logger::Draw(const char* title, bool* p_open)
 {
-    if (!ImGui::Begin(title, p_open))
-    {
-        ImGui::End();
-        return;
-    }
+    ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
+    if (!ImGui::Begin(title, p_open, ImGuiWindowFlags_MenuBar)) { ImGui::End(); return; }
 
-    // Options menu
-    if (ImGui::BeginPopup("Options"))
+    if (ImGui::BeginMenuBar())
     {
-        ImGui::Checkbox("Auto-scroll", &autoScroll);
-        ImGui::RadioButton("Debug",    &logLevel, 0);
-        ImGui::RadioButton("Info",     &logLevel, 1);
-        ImGui::RadioButton("Warn",     &logLevel, 2);
-        ImGui::RadioButton("Error",    &logLevel, 3);
-        ImGui::RadioButton("Critical", &logLevel, 4);
-        ImGui::EndPopup();
+        if (ImGui::BeginMenu("Settings"))
+        {
+            ImGui::MenuItem("Auto-scroll", NULL, &autoScroll);
+            ImGui::Separator();
+            if (ImGui::BeginMenu("Logging Levels"))
+            {
+                if (ImGui::BeginMenu("Colors"))
+                {
+                    ImGui::ColorEdit4("Debug", (float*)&color[0], ImGuiColorEditFlags_NoInputs);
+                    ImGui::ColorEdit4("Info",  (float*)&color[1], ImGuiColorEditFlags_NoInputs);
+                    ImGui::ColorEdit4("Warn",  (float*)&color[2], ImGuiColorEditFlags_NoInputs);
+                    ImGui::ColorEdit4("Error", (float*)&color[3], ImGuiColorEditFlags_NoInputs);
+                    ImGui::EndMenu();
+                }
+                ImGui::RadioButton("Debug", &logLevel, 0);
+                ImGui::RadioButton("Info",  &logLevel, 1);
+                ImGui::RadioButton("Warn",  &logLevel, 2);
+                ImGui::RadioButton("Error", &logLevel, 3);
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Edit"))
+        {
+            if (ImGui::MenuItem("Copy"))  ImGui::LogToClipboard();
+            if (ImGui::MenuItem("Clear")) Clear();
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
     }
-
-    // Main window
-    if (ImGui::Button("Options"))
-        ImGui::OpenPopup("Options");
-    ImGui::SameLine();
-    bool clear = ImGui::Button("Clear");
-    ImGui::SameLine();
-    bool copy = ImGui::Button("Copy");
-    ImGui::SameLine();
     filter.Draw("Filter", -100.0f);
 
     ImGui::Separator();
-    ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
-
-    if (clear) Clear();
-    if (copy)  ImGui::LogToClipboard();
+    ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);  
 
     ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
     const char* buf = buffer.begin();
     const char* buf_end = buffer.end();
     if (filter.IsActive())
     {
-        for (int line_no = 0; line_no < lineOffsets.Size; line_no++)
+        for (int line_no = 0; line_no < lineOffsets.size(); line_no++)
         {
-            const char* line_start = buf + lineOffsets[line_no];
-            const char* line_end = (line_no + 1 < lineOffsets.Size) ? (buf + lineOffsets[line_no + 1] - 1) : buf_end;
+            const char* line_start = buf + lineOffsets[line_no].lineNo;
+            const char* line_end = (line_no + 1 < lineOffsets.size()) ? (buf + lineOffsets[line_no + 1].lineNo - 1) : buf_end;
             if (filter.PassFilter(line_start, line_end))
-                ImGui::TextUnformatted(line_start, line_end);   // something else? for format and wrap
+            {
+                ImGui::PushStyleColor(ImGuiCol_Text, lineOffsets[line_no].color);
+                ImGui::TextUnformatted(line_start, line_end);
+                ImGui::PopStyleColor();
+            }
         }
     }
     else
     {
         ImGuiListClipper clipper;
-        clipper.Begin(lineOffsets.Size);
+        clipper.Begin(lineOffsets.size());
         while (clipper.Step())
         {
             for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
             {
-                const char* line_start = buf + lineOffsets[line_no];
-                const char* line_end = (line_no + 1 < lineOffsets.Size) ? (buf + lineOffsets[line_no + 1] - 1) : buf_end;
-                ImGui::TextUnformatted(line_start, line_end);    // something else? for format and wrap
+                const char* line_start = buf + lineOffsets[line_no].lineNo;
+                const char* line_end = (line_no + 1 < lineOffsets.size()) ? (buf + lineOffsets[line_no + 1].lineNo - 1) : buf_end;
+                ImGui::PushStyleColor(ImGuiCol_Text, lineOffsets[line_no].color);
+                ImGui::TextUnformatted(line_start, line_end);
+                ImGui::PopStyleColor();
             }
         }
         clipper.End();
--- src/Logger.h
+++ src/Logger.h
@@ -3,22 +3,49 @@
 
 class Logger
 {
+public:
+    enum class Level { DEBUG, INFO, WARN, ERROR, CRITICAL };
 private:
-    ImGuiTextBuffer buffer;
-    ImGuiTextFilter filter;
-    ImVector<int>   lineOffsets;
-    bool            autoScroll;
-    int             logLevel;
+    struct LineInfo
+    {
+        int    lineNo;
+        ImVec4 color;
+    };
+    ImGuiTextBuffer    buffer;
+    ImGuiTextFilter    filter;
+    ImVector<LineInfo> lineOffsets;
+    bool               autoScroll;
+    int                logLevel;
+    ImVec4             color[4] =
+    {
+        {1.0f, 1.0f, 1.0f, 1.0f},  // WHITE  by default
+        {0.4f, 1.0f, 0.4f, 1.0f},  // GREEN  by default
+        {1.0f, 1.0f, 0.4f, 1.0f},  // YELLOW by default
+        {1.0f, 0.4f, 0.4f, 1.0f}   // RED    by default
+    };
 
     Logger();
+    void Log(const Level lvl, const char* fmt, ...) IM_FMTARGS(2);
 public:
     static Logger* Instance();
 
-    enum class Level { DEBUG, INFO, WARN, ERROR, CRITICAL };
+    void SetLevel(const Level lvl) { logLevel = static_cast<int>(lvl); }
+    template <typename L>
+    void SetLevelColor(const L lvl, ImVec4 col)
+    {
+        if (lvl < Level::DEBUG || lvl > Level::ERROR) return;
+        color[static_cast<int>(lvl)] = col;
+    }
+
+    template<typename ...Args>
+    static void Debug(const char* fmt, Args... args) { Instance()->Log(Level::DEBUG, fmt, args...); }
+    template<typename ...Args>
+    static void Info(const char*  fmt, Args... args) { Instance()->Log(Level::INFO,  fmt, args...); }
+    template<typename ...Args>
+    static void Warn(const char*  fmt, Args... args) { Instance()->Log(Level::WARN,  fmt, args...); }
+    template<typename ...Args>
+    static void Error(const char* fmt, Args... args) { Instance()->Log(Level::ERROR, fmt, args...); }
 
     void Clear();
-    void Log(const Level lvl, const char* fmt, ...) IM_FMTARGS(2);
     void Draw(const char* title, bool* p_open = NULL);
-
-    void SetLevel(const Level lvl) { logLevel = static_cast<int>(lvl); }
 };
